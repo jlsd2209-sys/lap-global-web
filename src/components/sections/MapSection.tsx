010fd1cc4af6d53ad04cb66425584244c0e94b65
jlsd2209-sys <jlsd2209@gmail.com>
2026-03-04 20:51:46 -0300

    Update MapSection.tsx

--- src/components/sections/MapSection.tsx
+++ src/components/sections/MapSection.tsx
@@ -31,10 +31,10 @@ export const MapSection = () => {
   // ============================================================
   const movil = {
     altura: '600px',
-    titulo: { arriba: '20%', izquierda: '-10%', size: '1.8rem', sizeItalic: '1.1rem' },
+    titulo: { arriba: '20%', izquierda: '5%', size: '1.8rem', sizeItalic: '1.1rem' },
     mapa:   { arriba: '9%', derecha: '4%', tamaño: '310px', opacidad: '0.50' },
-    texto:  { arriba: '72.4%', izquierda: '-10%', ancho: '55%', size: '0.8rem' },
-    boton:  { abajo: '30%', izquierda: '10%', size: '9px' } // Cambiado a left para estabilidad
+    texto:  { arriba: '72.4%', izquierda: '5%', ancho: '55%', size: '0.8rem' },
+    boton:  { abajo: '30%', izquierda: '5%', size: '9px' } // Cambiado a left para estabilidad
   };
 
   const p = esMovil ? movil : escritorio;
