91f370ebd485005b5d64f75407d10f091ff079e3
jlsd2209-sys <jlsd2209@gmail.com>
2026-03-04 20:33:56 -0300

    Update MapSection.tsx

--- src/components/sections/MapSection.tsx
+++ src/components/sections/MapSection.tsx
@@ -32,8 +32,8 @@ export const MapSection = () => {
   const movil = {
     altura: '600px',
     titulo: { arriba: '20%', izquierda: '5%', size: '1.8rem', sizeItalic: '1.1rem' },
-    mapa:   { arriba: '9%', derecha: '4%', tamaño: '330px', opacidad: '0.50' },
-    texto:  { arriba: '73%', izquierda: '5%', ancho: '55%', size: '0.8rem' },
+    mapa:   { arriba: '9%', derecha: '4%', tamaño: '310px', opacidad: '0.50' },
+    texto:  { arriba: '72.4%', izquierda: '5%', ancho: '55%', size: '0.8rem' },
     boton:  { abajo: '30%', izquierda: '5%', size: '9px' } // Cambiado a left para estabilidad
   };
 
